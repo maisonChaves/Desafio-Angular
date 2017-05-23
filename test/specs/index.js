720408748b7d8d54b32170c754cdb7a244a74ecc
Maison Chaves <maison.chaves@p4sglobal.com>
2017-05-23 20:34:51 -0300

    Last layouts checks

--- test/specs/index.js
+++ test/specs/index.js
@@ -5,7 +5,8 @@ describe('desafio-angular', () => {
     before(function () {
         var chai = require('chai');
         var chaiAsPromised = require('chai-as-promised');
-        chai.use(chaiAsPromised);
+        var chaiWebdriver = require('chai-webdriverio').default;
+        chai.use(chaiWebdriver(chaiAsPromised));
         expect = chai.expect;
         chai.Should();
     });
@@ -14,7 +15,7 @@ describe('desafio-angular', () => {
         browser.url('/');
         expect(browser.getTitle()).to.be.equal('Desafio Angular');
     });
-    it('page 01', () => {
+    it('stepper', () => {
         browser.url('/');
         expect(browser.isEnabled('#next-1')).to.not.be.ok;
         $('#superhero').setValue('test1');
@@ -23,11 +24,28 @@ describe('desafio-angular', () => {
         $('#quote').setValue('quote');
         expect(browser.isEnabled('#next-1')).to.be.ok;
         $('#next-1').click();
-    });
-    it('page 02', () => {
+
         expect(browser.isEnabled('#next-2')).to.not.be.ok;
+        $('#movie-0').waitForExist(5000);
         $('#movie-0').click();
         expect(browser.isEnabled('#next-2')).to.be.ok;
         $('#next-2').click();
+
+        /*
+        expect(browser.isEnabled('#next-3')).to.not.be.ok;
+        $('#season-5').waitForExist(10 * 1000);
+
+        $('#serie-title').waitForText(10 * 1000);
+        expect('#serie-title').to.have.text('Game of Thrones (2011–)');
+        
+        $('#season-5').click();
+        expect(browser.isEnabled('#next-3')).to.be.ok;
+        $('#next-3').click();
+
+        expect(browser.isEnabled('#next-4')).to.not.be.ok;
+        $('#food-any').click();
+        expect(browser.isEnabled('#next-4')).to.be.ok;
+        $('#next-4').click();
+        */
     });
 });
